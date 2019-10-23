4284fcde15b74126adda5c62e5b5eecaf2ea7202
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-10-23 17:47:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   reverse_sentence=[]
-  new_sentence=string.split("")
+  new_sentence=string.split(" ")
   new_sentence.collect do |words|
     reverse_sentence<< words.reverse 
    end 
-    reverse_sentence.join("")
+    reverse_sentence.join(" ")
   end 
